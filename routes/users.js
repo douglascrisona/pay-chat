3ba3ae4284e218adad434a05f85f3f3e0f09a591
Douglas Crisona <douglascrisona@gmail.com>
2016-07-28 11:12:39 -0700

    Routes connect to users database and change data. #1

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,73 @@
 var express = require('express')
 var users = express.Router()
+var MongoClient = require('mongodb').MongoClient
+
+var url = 'mongodb://localhost:27017/users'
+
 
 users.post('/:name/:password/:email/:number', function(req, res) {
+  console.log(newUser)
+  var newUser = {}
+  newUser.name = req.params.name;
+  newUser.password = req.params.password;
+  newUser.email = req.params.email;
+  newUser.number = req.params.number;
+
+  MongoClient.connect(url, function(err, db) {
+  if(err) {
+    console.log('Not Connected')
+  } else {
+    console.log('Connected correctly to server')
+  }
+  var users = db.collection('users')
+  users
+    .insertMany([
+      newUser
+    ],
+    function(err, result) {
+      db.close()
+    }
+  )
+  });
   res.send();
 });
 
 users.get('/all', function(req, res) {
-  res.send();
+  MongoClient.connect(url, function(err, db) {
+    if(err) {
+      console.log('Not Connected')
+    } else {
+      console.log('Connected correctly to server')
+    }
+    var users = db.collection('users');
+    users
+      .find({})
+      .toArray(function(err, docs) {
+        res.send(docs);
+        db.close();
+      });
+  });
 });
 
 users.delete('/:name', function(req, res) {
+  var user = {}
+  user.name = req.params.name;
+
+  MongoClient.connect(url, function(err, db) {
+    if(err) {
+      console.log('Not Connected')
+    } else {
+      console.log('Connected correctly to server')
+    }
+    var users = db.collection('users');
+    users
+    .deleteOne(
+      {name: user.name},
+      function(err, result) {
+        db.close()
+      }
+    )
+  });
   res.send();
 });
 
