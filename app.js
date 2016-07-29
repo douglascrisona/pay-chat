a1b38d928179e1b8e7c4da9a72b34c47184e5186
Douglas Crisona <douglascrisona@gmail.com>
2016-07-29 10:15:37 -0700

    Serves up angular files to front-end. #1

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ var app = express();
 var users = require('./routes/users.js');
 app.use('/users', users);
 
+app.get('/public/app.js', function(req, res) {
+  res.sendFile(__dirname + '/public/app.js')
+});
+
+app.get('/public/paychat/register.controller.js', function(req, res) {
+  res.sendFile(__dirname + '/public/paychat/register.controller.js')
+});
+
+app.use(express.static('./public'))
 
 app.listen(8080)
