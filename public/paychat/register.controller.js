1ae34e7b876a05a59165d6df9c04345e283a6e6d
Douglas Crisona <douglascrisona@gmail.com>
2016-07-30 10:35:25 -0700

    Added recipient data and request to controller. #2

--- public/paychat/register.controller.js
+++ public/paychat/register.controller.js
@@ -20,9 +20,14 @@ function paychat($http) {
   }
 
   vm.newUser = function(item) {
-    console.log(item)
     var user = item
     var add = $http.post('http://localhost:8080/users/' + user.name + '/' + user.password + '/' + user.email + '/' + user.phone)
     add
+  }
+
+  vm.recipients = function() {
+    var view = $http.get('http://localhost:8080/recipients/Doug/').then(function successCallback(response) {
+      vm.contacts = response.data
+    })
   }
 }
