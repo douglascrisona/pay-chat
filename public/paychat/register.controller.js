7821757800a8adc3a913781ed5419b1fa9ca90b4
Douglas Crisona <douglascrisona@gmail.com>
2016-08-02 10:17:07 -0700

    Added angular http routes to log user in. #5

--- public/paychat/register.controller.js
+++ public/paychat/register.controller.js
@@ -2,6 +2,7 @@ var app = angular.module('paychat');
 
 app.controller('paychatController', paychat);
 
+
 paychat.$inject = ['$http'];
 
 function paychat($http) {
@@ -11,9 +12,6 @@ function paychat($http) {
   function activate() {
     register();
   }
-
-  vm.loggedin = true;
-
 
   function register($scope) {
 
@@ -29,8 +27,6 @@ function paychat($http) {
   vm.recipients = function() {
     var view = $http.get('http://localhost:8080/recipients/Doug/').then(function successCallback(response) {
       vm.contacts = response.data
-      console.log(vm.contacts)
-
       vm.showRecipients = true
       vm.showInvoices = false
       vm.showNewInvoice = false;
@@ -43,7 +39,6 @@ function paychat($http) {
     user.recipient = name;
     user.email = email;
     user.phone = phone;
-    console.log(user)
     var add = $http.post('http://localhost:8080/recipients/Doug/' + user.recipient + '/' + user.email + '/' + user.phone)
     add
       .then(function() {
@@ -52,14 +47,12 @@ function paychat($http) {
   }
 
   vm.createInvoice = function(invoice) {
-    console.log(invoice)
     var create = $http.post('http://localhost:8080/invoices/' + invoice.name + '/' + invoice.id + '/' + invoice.details + '/' + invoice.qty + '/' + invoice.cost + '/' + invoice.total + '/' + invoice.recipient)
     create
   }
 
 
   vm.displayNewInvoice = function() {
-    console.log('hello')
     vm.showRecipients = false;
     vm.showInvoices = false
     vm.showNewInvoice = true;
@@ -70,12 +63,37 @@ function paychat($http) {
   vm.viewInvoices = function() {
     var view = $http.get('http://localhost:8080/invoices/myinvoices/Juan').then(function successCallback(response) {
       vm.invoice = response.data
-      console.log(vm.invoice)
 
       vm.invoices = true;
       vm.showRecipients = false;
       vm.showNewInvoice = false;
       vm.showInvoices = false;
+    });
+  }
+
+  vm.login = function(user) {
+    var login = $http.get('http://localhost:8080/login/' + user.name + '/' + user.password )
+    vm.name = user.name
+    login
+    .then(function() {
+      vm.check(vm.name)
+    }).then(function() {
+      vm.valid()
+    })
+  }
+
+  vm.check = function(user) {
+    var check = $http.post('http://localhost:8080/login/' + user).then(function successCallback(response) {
+    });
+  }
+
+  vm.valid = function() {
+    vm.loggedin = true;
+  }
+
+  vm.getSession = function() {
+    var session = $http.post('http://localhost:8080/login/session').then(function successCallback(response) {
+      vm.sessionId = response.data
     });
   }
 }
